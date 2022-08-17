66a61efb1fc7d6c01d6f36081979df403408a05f
andres jaramillo <andres.jaramillo@celuweb.com>
2022-08-17 13:27:32 -0500

    Project configuration

---
+++ ZemogaTest/ZemogaTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZemogaTest
+//
+//  Created by andres jaramillo on 17/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
